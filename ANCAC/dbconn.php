e5406618ff768e36171dcf49d3083c0e9eac9730
jeremy <jeremy@monolith>
2014-02-23 18:40:18 -0600

    fix db name

--- ANCAC/dbconn.php
+++ ANCAC/dbconn.php
@@ -1,4 +1,4 @@
 <?php
     $connect = mysql_connect('localhost', 'ancac', 'Me6XNywHbtEuQS4N') or die("could not connect to server");
-    $db_select = @mysql_select_db('13874_alabamacacsorg') or die("could not select the database");
+    $db_select = @mysql_select_db('ancac') or die("could not select the database");
 ?>
