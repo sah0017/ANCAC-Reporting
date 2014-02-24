c7bb961d79c2d1d8ca0fba27b57f83a1a01d1f3a
jeremy <jeremy@monolith>
2014-02-23 18:34:17 -0600

    db connection

--- ANCAC/dbconn.php
+++ ANCAC/dbconn.php
@@ -1,4 +1,4 @@
 <?php
-    $connect = mysql_connect('mysql1.myregisteredsite.com', '13874_user122442', 'fq5gc2bpkp') or die("could not connect to server");
+    $connect = mysql_connect('localhost', 'ancac', 'Me6XNywHbtEuQS4N') or die("could not connect to server");
     $db_select = @mysql_select_db('13874_alabamacacsorg') or die("could not select the database");
 ?>
