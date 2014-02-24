f9c8cf7e9121cd56fdbf5be3795407f271941c79
jeremy <www@ubuntuVM>
2014-02-23 20:51:44 -0600

    final hasher

--- ANCAC/hasher.php
+++ ANCAC/hasher.php
@@ -1,4 +1,4 @@
-<?php
+<?
 require("./dbconn.php");
 require("./PasswordHash.php");
 
@@ -6,13 +6,15 @@ $hasher = new PasswordHash(8, false);
 
 $sql = "SELECT password FROM directors";
 $result = @mysql_query($sql) or mysql_error();
-
+echo($result);
 while ($row = mysql_fetch_object($result)) {
 	
 	
 	$hash=$hasher->HashPassword($row->password);
-	
+echo($hash."  "."|".$row->password."|");
 	$sqlUpdate = "UPDATE directors SET password = '".$hash."' WHERE password = '".$row->password."'";
+//	echo($sqlUpdate);
 	$resultUpdate = @mysql_query($sqlUpdate) or mysql_error();
+	echo($resultUpdate);
 }
 ?>
