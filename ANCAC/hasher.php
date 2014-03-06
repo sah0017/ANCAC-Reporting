8fee3c0562139f05a4ce9ae02048841cee996f42
jeremy <jeremy@monolith>
2014-03-06 01:05:49 -0600

    revert

--- ANCAC/hasher.php
+++ ANCAC/hasher.php
@@ -6,13 +6,6 @@ echo("checking for backup table... ");
 $result = mysql_query("SHOW TABLES LIKE 'directors_BACKUP'");
 $tableExists = mysql_num_rows($result) > 0;
 
-if(!$tableExists){
-        echo(" making backup... ");
-        $sqlCreate="CREATE TABLE `ancac`.`directors_BACKUP` ( `RID` int( 11 ) NOT NULL AUTO_INCREMENT ,`name` varchar( 40 ) NOT NULL DEFAULT '',`username` varchar( 12 ) NOT NULL DEFAULT '',`email` varchar( $
-        mysql_query($sqlCreate);
-        $sqlSet="SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO';";
-        mysql_query($sqlSet);  
-        $sqlInsert="INSERT INTO `ancac`.`directors_BACKUP` SELECT * FROM `ancac`.`directors`;[...]";
         $hasher = new PasswordHash(8, false);
         echo(" changing table... ");
         $sqlAlter = "ALTER TABLE `directors` CHANGE `password` `password` VARCHAR( 72 ) NOT NULL DEFAULT''";
@@ -28,8 +21,6 @@ if(!$tableExists){
         //      echo($sqlUpdate);
                 mysql_query($sqlUpdate);
         }
-}
-else
-        echo(" passwords already hashed.");
+
 echo(" finished");
 ?>
