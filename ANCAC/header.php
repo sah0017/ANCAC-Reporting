741269d5905ba0c79c957a7869a48735524e2b69
jbmur_000 <jbmur_000@Jonathan-pc>
2014-04-10 13:50:19 -0500

    test

--- ANCAC/header.php
+++ ANCAC/header.php
@@ -3,14 +3,14 @@
 	//NEW WAY OF CONNECTING TO DATABASE
 	
 	// Include ezSQL core
-	include_once "ez_sql_core.php";
+	//include_once //"ez_sql_core.php";
 	
 	// Include ezSQL database specific component (in this case mySQL)
-	include_once "ez_sql_mysqli.php";
+	//include_once "ez_sql_mysqli.php";
 	
 	// Initialise database object and establish a connection
 	// at the same time - db_user / db_password / db_name / db_host
-	$db = new ezSQL_mysqli('ancac','','ancac','localhost');
+	//$db = new ezSQL_mysqli('ancac','','ancac','localhost');
 	// JM
 ?>
 
