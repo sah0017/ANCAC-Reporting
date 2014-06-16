b86462503b34ec2bdd30512c5f71b4639e7356ac
ErichErnesto <ErichErnesto@Ernesto>
2014-06-15 21:20:42 -0500

    Add the header to the table for excel Export

--- ANCAC/excelExport.php
+++ ANCAC/excelExport.php
@@ -25,14 +25,8 @@ require($root."dbconn.php");
 	$emptyArray = array();
 	//$memberArray = array();
 
-//sql to pull the information on all 'Full Member' centers
-
-	$sqlFullMember = "SELECT center, CenterName FROM centers WHERE center not in (0,99) AND centerlevel = 'Full Member' order by center";
-
-	$numRecFullMember = $db->get_results($sqlFullMember);
-	
-//Add headers from GrandTotal to the Excel Export
-        $grandTotalheader = array('Tot.SVCS.' , 'Center + FI' , 'E.F.A' , 'I.C.S' , 'Exam' , 'Pros.' , 'M.D.T.' , 'T.C.S.' , '0-6' , '7-12' , '13-18' , 'M' , 'F' , 'AA' , 'ASN' , 'CAU.' , 'HISP.' , 'QTR');
+        //Add headers from GrandTotal to the Excel Export
+        $grandTotalheader = array( ' ', 'Center' , 'QTR' , 'Tot.SVCS.' , 'FI' , 'E.F.A' , 'I.C.S' , 'Exam' , 'Pros.' , 'M.D.T.' , 'T.C.S.' , '0-6' , '7-12' , '13-18' , 'M' , 'F' , 'AA' , 'ASN' , 'CAU.' , 'HISP.');
         
        fputcsv($fp, (array)$grandTotalheader);
        fputcsv($fp, $emptyArray);
@@ -48,22 +42,36 @@ require($root."dbconn.php");
         
         }//end of foreach
        */
-                
+       
+//sql to pull the information on all 'Full Member' centers
+
+       
+	$sqlFullMember = "SELECT center, CenterName FROM centers WHERE center not in (0,99) AND centerlevel = 'Full Member' order by center";
+
+	$numRecFullMember = $db->get_results($sqlFullMember);
+	       
                 
 	foreach ($numRecFullMember as $rowFullMember) 
 	{
-		
-		$sqlLoop =  "SELECT fiTotal, fi0to6, fi7to12, fi13to18, fiMale, fiFemale, fiAfrAmerican, fiAsian, fiCauc, fiHispanic, fiOther, extForenEval, ".
+          
+            //put quater in the first colum
+		$sqlLoop =  "SELECT actualPerfStats.quarter , fiTotal, fi0to6, fi7to12, fi13to18, fiMale, fiFemale, fiAfrAmerican, fiAsian, fiCauc, fiHispanic, fiOther, extForenEval, ".
+					"intCounsSes, totCounSes, multDisTeamMeet, prosCases, medExamRef FROM actualPerfStats JOIN actualExpenditures
+					 		ON actualPerfStats.center = actualExpenditures.center AND actualPerfStats.fiscalyear = actualExpenditures.fiscalyear AND 
+							actualPerfStats.quarter = actualExpenditures.quarter WHERE actualPerfStats.center = '$rowFullMember->center' 
+									AND actualPerfStats.fiscalyear = '$fiscalYear' AND actualExpenditures.completed = 'COM' ORDER BY actualPerfStats.quarter";
+		/*$sqlLoop =  "SELECT fiTotal, fi0to6, fi7to12, fi13to18, fiMale, fiFemale, fiAfrAmerican, fiAsian, fiCauc, fiHispanic, fiOther, extForenEval, ".
 					"intCounsSes, totCounSes, multDisTeamMeet, prosCases, medExamRef, actualPerfStats.quarter FROM actualPerfStats JOIN actualExpenditures
 					 		ON actualPerfStats.center = actualExpenditures.center AND actualPerfStats.fiscalyear = actualExpenditures.fiscalyear AND 
 							actualPerfStats.quarter = actualExpenditures.quarter WHERE actualPerfStats.center = '$rowFullMember->center' 
 									AND actualPerfStats.fiscalyear = '$fiscalYear' AND actualExpenditures.completed = 'COM' ORDER BY actualPerfStats.quarter";
 		//echo $sqlLoop."<br>";
-		
-		fputcsv($fp, (array)$rowFullMember);
+		*/
+		//fputcsv($fp, (array)$rowFullMember);
 		
 		$resultLoop = $db->get_row($sqlLoop);
-		fputcsv($fp, (array)$resultLoop);
+                //name and result in same line
+		fputcsv($fp,(array)$rowFullMember + (array)$resultLoop);
 		fputcsv($fp, $emptyArray);
 	}
 	fputcsv($fp, $emptyArray);
@@ -77,18 +85,24 @@ require($root."dbconn.php");
 		
 	foreach ($numRecAssociate as $rowAssociate) 
 	{
+            
+            $sqlLoop =  "SELECT actualPerfStats.quarter, fiTotal, fi0to6, fi7to12, fi13to18, fiMale, fiFemale, fiAfrAmerican, fiAsian, fiCauc, fiHispanic, fiOther, extForenEval, ".
+					"intCounsSes, totCounSes, multDisTeamMeet, prosCases, medExamRef FROM actualPerfStats JOIN actualExpenditures
+					 		ON actualPerfStats.center = actualExpenditures.center AND actualPerfStats.fiscalyear = actualExpenditures.fiscalyear AND 
+							actualPerfStats.quarter = actualExpenditures.quarter WHERE actualPerfStats.center = '$rowAssociate->center' 
+									AND actualPerfStats.fiscalyear = '$fiscalYear' AND actualExpenditures.completed = 'COM' ORDER BY actualPerfStats.quarter";
 		
-		$sqlLoop =  "SELECT fiTotal, fi0to6, fi7to12, fi13to18, fiMale, fiFemale, fiAfrAmerican, fiAsian, fiCauc, fiHispanic, fiOther, extForenEval, ".
+		/*$sqlLoop =  "SELECT fiTotal, fi0to6, fi7to12, fi13to18, fiMale, fiFemale, fiAfrAmerican, fiAsian, fiCauc, fiHispanic, fiOther, extForenEval, ".
 					"intCounsSes, totCounSes, multDisTeamMeet, prosCases, medExamRef, actualPerfStats.quarter FROM actualPerfStats JOIN actualExpenditures
 					 		ON actualPerfStats.center = actualExpenditures.center AND actualPerfStats.fiscalyear = actualExpenditures.fiscalyear AND 
 							actualPerfStats.quarter = actualExpenditures.quarter WHERE actualPerfStats.center = '$rowAssociate->center' 
 									AND actualPerfStats.fiscalyear = '$fiscalYear' AND actualExpenditures.completed = 'COM' ORDER BY actualPerfStats.quarter";
 		//echo $sqlLoop."<br>";
-	
-		fputcsv($fp, (array)$rowAssociate);
+                */
+		//fputcsv($fp, (array)$rowAssociate);
 		
 		$resultLoop = $db->get_row($sqlLoop);
-		fputcsv($fp, (array)$resultLoop);
+		fputcsv($fp,(array)$rowAssociate + (array)$resultLoop);
 		fputcsv($fp, $emptyArray);
 
 	}
@@ -103,16 +117,22 @@ require($root."dbconn.php");
 			
 	foreach ($numRecPilot as $rowPilot) 
 	{
-		$sqlLoop =  "SELECT fiTotal, fi0to6, fi7to12, fi13to18, fiMale, fiFemale, fiAfrAmerican, fiAsian, fiCauc, fiHispanic, fiOther, extForenEval, ".
+            $sqlLoop =  "SELECT actualPerfStats.quarter , fiTotal, fi0to6, fi7to12, fi13to18, fiMale, fiFemale, fiAfrAmerican, fiAsian, fiCauc, fiHispanic, fiOther, extForenEval, ".
+					"intCounsSes, totCounSes, multDisTeamMeet, prosCases, medExamRef FROM actualPerfStats JOIN actualExpenditures
+					 		ON actualPerfStats.center = actualExpenditures.center AND actualPerfStats.fiscalyear = actualExpenditures.fiscalyear AND 
+							actualPerfStats.quarter = actualExpenditures.quarter WHERE actualPerfStats.center = '$rowPilot->center' 
+									AND actualPerfStats.fiscalyear = '$fiscalYear' AND actualExpenditures.completed = 'COM' ORDER BY actualPerfStats.quarter";
+            
+		/*$sqlLoop =  "SELECT fiTotal, fi0to6, fi7to12, fi13to18, fiMale, fiFemale, fiAfrAmerican, fiAsian, fiCauc, fiHispanic, fiOther, extForenEval, ".
 					"intCounsSes, totCounSes, multDisTeamMeet, prosCases, medExamRef, actualPerfStats.quarter FROM actualPerfStats JOIN actualExpenditures
 					 		ON actualPerfStats.center = actualExpenditures.center AND actualPerfStats.fiscalyear = actualExpenditures.fiscalyear AND 
 							actualPerfStats.quarter = actualExpenditures.quarter WHERE actualPerfStats.center = '$rowPilot->center' 
 									AND actualPerfStats.fiscalyear = '$fiscalYear' AND actualExpenditures.completed = 'COM' ORDER BY actualPerfStats.quarter";
-			
-		fputcsv($fp, (array)$rowPilot);
+			*/
+		//fputcsv($fp, (array)$rowPilot);
 		
 		$resultLoop = $db->get_row($sqlLoop);
-		fputcsv($fp, (array)$resultLoop);
+		fputcsv($fp,(array)$rowPilot + (array)$resultLoop);
 		fputcsv($fp, $emptyArray);
 	}
 			
